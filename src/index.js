5b9b2734a609573d7beb8a336515c8b75164d30a
Terry Demco <29758222+terrydemco@users.noreply.github.com>
2019-08-02 17:29:26 -0600

    rework test

--- src/index.js
+++ src/index.js
@@ -17,18 +17,51 @@ const platformName = 'homebridge-hdfury-integral2-rs232';
 const platformPrettyName = 'Integral2';
 
 module.exports = (homebridge) => {
-  HOMEBRIDGE.Accessory = homebridge.platformAccessory;
+  //HOMEBRIDGE.Accessory = homebridge.platformAccessory;
   HOMEBRIDGE.Service = homebridge.hap.Service;
   HOMEBRIDGE.Characteristic = homebridge.hap.Characteristic;
-  HOMEBRIDGE.UUIDGen = homebridge.hap.uuid;
-  HOMEBRIDGE.homebridge = homebridge;
+  //HOMEBRIDGE.UUIDGen = homebridge.hap.uuid;
+  //HOMEBRIDGE.homebridge = homebridge;
 
-  homebridge.registerPlatform(platformName, platformPrettyName, EpsonProjectorPlatform, false);
+  //homebridge.registerPlatform(platformName, platformPrettyName, EpsonProjectorPlatform, false);
+  homebridge.registerAccessory("homebridge-hdfury-integral2-rs232", "Integral2", mySwitch)
 
-
-  ProjectorInputServiceTypes.registerWith(homebridge.hap);
+  //ProjectorInputServiceTypes.registerWith(homebridge.hap);
 };
 
+function mySwitch(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+
+mySwitch.prototype = {
+
+	getServices: function() {
+		 let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
+      .setCharacteristic(Characteristic.Model, "My switch model")
+      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+
+    let switchService = new Service.Switch("My switch");
+    switchService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getSwitchOnCharacteristic.bind(this))
+      .on('set', this.setSwitchOnCharacteristic.bind(this));
+
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+	}
+	
+	getSwitchOnCharacteristic: function (next) {
+	}
+	setSwitchOnCharacteristic: function (on, next) {
+	}
+
+
+}
 const EpsonProjectorPlatform = class {
   constructor(log, config, api) {
     this.log = log;
