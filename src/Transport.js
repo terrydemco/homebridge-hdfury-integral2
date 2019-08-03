ddbef8eb0b705e0b5e5efb756e4b5a1e3813c15e
Terry Demco <29758222+terrydemco@users.noreply.github.com>
2019-08-02 19:31:03 -0600

    oau

--- src/Transport.js
+++ src/Transport.js
@@ -93,7 +93,6 @@ class Transport extends EventEmitter {
 
   _execute(cmd, timeout) {
     return this._taskQueue.push(async () => {
-      await this._drainAndFlush();
       const commandId = this._command++;
 
       let response = null;
